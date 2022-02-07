0f2a3a358683f9c664107159cb4ae6ca42af428e
rodrigopereza <jr.pereza@hotmail.com>
2022-02-07 09:26:09 -0500

    Fix validacion registro

--- bd/chequeoYaExisteUsuario.go
+++ bd/chequeoYaExisteUsuario.go
@@ -8,25 +8,25 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func ChequeoYaExisteUsuario(email string) (models.Usuario, bool, error) {
+func ChequeoYaExisteUsuario(email string) (models.Usuario, bool, string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 
 	defer cancel()
 
 	db := MongoCN.Database("twitter")
-	col := db.Collection("usuario")
+	col := db.Collection("usuarios")
 
 	condition := bson.M{"email": email}
 
 	var resultado models.Usuario
 
 	err := col.FindOne(ctx, condition).Decode(&resultado)
-	//ID := resultado.ID.Hex()
+	ID := resultado.ID.Hex()
 
 	if err != nil {
-		return resultado, false, err
+		return resultado, false, ID
 	}
 
-	return resultado, true, nil
+	return resultado, true, ID
 
 }
